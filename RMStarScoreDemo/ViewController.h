c330199a379e338b3e8cd26ebdf1f4075f0f05b8
chihaodong <fychihaodong@163.com>
2015-05-21 15:57:35 +0800

    RMStarScoreDemo
    RMStarScoreDemo 核心文件

---
+++ RMStarScoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RMStarScoreDemo
+//
+//  Created by kiwi on 15/5/21.
+//  Copyright (c) 2015年 chd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
